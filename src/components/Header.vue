f3e01e23dd61e75a92a34f795ab497bc8dbd9bb2
hideaki-umizaru <momoirosoccer@gmail.com>
2021-02-09 19:47:37 +0900

    first-commit

---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+<header class="header">
+  <div id="hamburger" @click="hamburger()" :class="{'active':isClass}">
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
+</header> 
+</template>
+
+<script>
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
